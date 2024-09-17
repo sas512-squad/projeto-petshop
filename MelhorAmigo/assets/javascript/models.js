1f829d1bcf5f8138803969c277fa30f43b3de5c5
João Victor <jsouzalimasilva@gmail.com>
2024-09-16 21:14:17 -0300

    Alteração de imagens de produtos para .png

--- MelhorAmigo/assets/javascript/models.js
+++ MelhorAmigo/assets/javascript/models.js
@@ -1,31 +1,31 @@
 let modelsJson = [
-    {id:1, name:'Ração GoldeN', img:"/img/RSpecialDog.jpg", price:[13.90, 25.00, 60.00], sizes:['500g', '1kg', '2,5kg'], description:'Ração Special Dog Premium Carne Plus para Cães Adultos', category: 'ração'},
+    {id:1, name:'Ração GoldeN', img:"/img/RSpecialDog.png", price:[13.90, 25.00, 60.00], sizes:['500g', '1kg', '2,5kg'], description:'Ração Special Dog Premium Carne Plus para Cães Adultos', category: 'ração'},
 
-    {id:2, name:'Ração Farmina N&D Ancestral Grain', img:"/img/RaçãoFormina.jpg", price:[126.90, 210.00, 422.99], sizes:['3kg', '5kg', '10kg'], description:'Ração Farmina N&D Ancestral Grain para Cães Adultos de Raças Grandes Sabor Cordeiro e Blueberry', category: 'ração'},
+    {id:2, name:'Ração Farmina N&D Ancestral Grain', img:"/img/RaçãoFormina.png", price:[126.90, 210.00, 422.99], sizes:['3kg', '5kg', '10kg'], description:'Ração Farmina N&D Ancestral Grain para Cães Adultos de Raças Grandes Sabor Cordeiro e Blueberry', category: 'ração'},
     
-    {id:3, name:'Ração Guabi Natural Grain Free', img:"/img/RGuabiNatural.jpg", price:[60.00, 85.00, 109.90], sizes:['3kg', '5kg', '10kg'], description:'Ração Guabi Natural Grain Free para Gatos Adultos Castrados Sabor Salmão e Lentilha', category: 'ração'},
+    {id:3, name:'Ração Guabi Natural Grain Free', img:"/img/RGuabiNatural.png", price:[60.00, 85.00, 109.90], sizes:['3kg', '5kg', '10kg'], description:'Ração Guabi Natural Grain Free para Gatos Adultos Castrados Sabor Salmão e Lentilha', category: 'ração'},
 
-    {id:4, name:'Ração FreshMeat', img:"/img/RFreshMeat.jpg", price:[32.90, 72.25, 140.99], sizes:['3kg', '5kg', '10kg'], description:'Ração Fórmula Natural FreshMeat para Gatos Adultos Castrados Sabor Salmão', category: 'ração'},
+    {id:4, name:'Ração FreshMeat', img:"/img/RFreshMeat.png", price:[32.90, 72.25, 140.99], sizes:['3kg', '5kg', '10kg'], description:'Ração Fórmula Natural FreshMeat para Gatos Adultos Castrados Sabor Salmão', category: 'ração'},
 
-    {id:5, name:'Ração N&D Grain Free', img:"/img/RN&DGrainFree.jpg", price:[180.40, 250.60, 470.90], sizes:['3kg', '5kg', '10kg'], description:'Ração N&D Grain Free para Cães Filhotes de Raças Grandes sabor Frango e Romã', category: 'ração'},
+    {id:5, name:'Ração N&D Grain Free', img:"/img/RN&DGrainFree.png", price:[180.40, 250.60, 470.90], sizes:['3kg', '5kg', '10kg'], description:'Ração N&D Grain Free para Cães Filhotes de Raças Grandes sabor Frango e Romã', category: 'ração'},
 
-    {id:6, name:'Brinquedo Macaco Petz', img:"/img/img-produto04.jpg", price:[15.90, 25.00, 30.99], sizes:[' ', ' ', ' '], description:' ', category: 'brinquedo'},
+    {id:6, name:'Brinquedo Macaco Petz', img:"/img/img-produto04.png", price:[15.90, 25.00, 30.99], sizes:[' ', ' ', ' '], description:' ', category: 'brinquedo'},
 
-    {id:7, name:'Brinquedo Líder Pet Bola Cravo para Cães', img:"/img/Brinquedo1.jpg", price:[10.00, 22.00, 42.90], sizes:['x3', 'x8', 'x12'], description:'Cores Sortidas', category: 'brinquedo'},
+    {id:7, name:'Brinquedo Líder Pet Bola Cravo para Cães', img:"/img/Brinquedo1.png", price:[10.00, 22.00, 42.90], sizes:['x3', 'x8', 'x12'], description:'Cores Sortidas', category: 'brinquedo'},
 
-    {id:8, name:'Pet Games Labirinto Verde Água', img:"/img/Brinquedo2.jpg", price:[63.00, 87.50, 109.90], sizes:['', '', ''], description:'Brinquedo e Comedouro para Cães e Gatos', category: 'brinquedo'},
+    {id:8, name:'Pet Games Labirinto Verde Água', img:"/img/Brinquedo2.png", price:[63.00, 87.50, 109.90], sizes:['', '', ''], description:'Brinquedo e Comedouro para Cães e Gatos', category: 'brinquedo'},
 
-    {id:9, name:'Capivara de Pelúcia', img:"/img/Brinquedo3.jpg", price:[39.40, 72.45, 99.90], sizes:['', '', ''], description:'Brinquedo Petz para Cães', category: 'brinquedo'},
+    {id:9, name:'Capivara de Pelúcia', img:"/img/Brinquedo3.png", price:[39.40, 72.45, 99.90], sizes:['', '', ''], description:'Brinquedo Petz para Cães', category: 'brinquedo'},
 
-    {id:10, name:'Brinquedo Piranha de Pelúcia Petz para Cães', img:"/img/Brinquedo5.jpg", price:[299.50, 310.00, 470.90], sizes:['', '', ''], description:'', category: 'brinquedo'},
+    {id:10, name:'Brinquedo Piranha de Pelúcia Petz para Cães', img:"/img/Brinquedo5.png", price:[299.50, 310.00, 470.90], sizes:['', '', ''], description:'', category: 'brinquedo'},
 
-    {id:11, name:'Eliminador de Odores Lavanda UP Clean', img:"/img/Produto1jpg.jpg", price:[8.50, 13.90, 16.0], sizes:['1L', '2L', '2,5L'], description:'Para cães e Gatos', category: 'higiene'},
+    {id:11, name:'Eliminador de Odores Lavanda UP Clean', img:"/img/Produto1jpg.png", price:[8.50, 13.90, 16.0], sizes:['1L', '2L', '2,5L'], description:'Para cães e Gatos', category: 'higiene'},
 
-    {id:12, name:'Hidratante de Patas e Focinho', img:"/img/Produto2.jpg", price:[38.90, 42.90, 52.90], sizes:['30g', '70g', '90g'], description:'Somente para Cães', category: 'higiene'},
+    {id:12, name:'Hidratante de Patas e Focinho', img:"/img/Produto2.png", price:[38.90, 42.90, 52.90], sizes:['30g', '70g', '90g'], description:'Somente para Cães', category: 'higiene'},
 
-    {id:13, name:"Dug's Coleira Antiparasitario", img:"/img/Produto3.jpg", price:[85.80, 92.99, 109.90], sizes:['Porte', 'Porte', 'Porte'], description:'', category: 'higiene'},
+    {id:13, name:"Dug's Coleira Antiparasitario", img:"/img/Produto3.png", price:[85.80, 92.99, 109.90], sizes:['Porte', 'Porte', 'Porte'], description:'', category: 'higiene'},
 
-    {id:14, name:'Hectotrio spot GG cães', img:"/img/Produto4.jpg", price:[25.70, 43.50, 72.45], sizes:['8kg', '15kg', '24kg'], description:'Somente para Cães', category: 'higiene'},
+    {id:14, name:'Hectotrio spot GG cães', img:"/img/Produto4.png", price:[25.70, 43.50, 72.45], sizes:['8kg', '15kg', '24kg'], description:'Somente para Cães', category: 'higiene'},
 
-    {id:15, name:'Hectotrio spot FP gato', img:"/img/Produto5.jpg", price:[298.90, 350.80, 470.90], sizes:['2kg', '3kg', '4kg'], description:'Somente para Gatos', category: 'higiene'},
+    {id:15, name:'Hectotrio spot FP gato', img:"/img/Produto5.png", price:[298.90, 350.80, 470.90], sizes:['2kg', '3kg', '4kg'], description:'Somente para Gatos', category: 'higiene'},
 ];
